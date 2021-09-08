d0a71244b8ade93905050a0b662fc287f7c694e4
Sascha Homeier <sascha.homeier@gmail.com>
2021-09-08 21:16:56 +0700

    Module 3 - Refactored name of invokeAny example

--- src/main/java/io/github/shomeier/module4/SingleInvokeAnyExample.java
+++ src/main/java/io/github/shomeier/module4/SingleInvokeAnyExample.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class SingleInvokeExample {
+public class SingleInvokeAnyExample {
 
     public static void main(String[] args) {
 
